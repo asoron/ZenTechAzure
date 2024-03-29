bfca80ef1904311ba33a02f8c40a0d7df8a56377
asoron <34812088+asoron@users.noreply.github.com>
2024-03-29 16:45:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
+// vite.config.js
+
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			external: ["@azure/openai"],
+		},
+	},
 });
